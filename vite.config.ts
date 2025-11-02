677933556edbbfcc567007d0b3aff1ba0dcfd6a7
Wuzhong <wuzhong@orderly.network>
2025-11-02 23:31:47 +0800

    remove redundant

--- vite.config.ts
+++ vite.config.ts
@@ -8,9 +8,7 @@ export default defineConfig({
   // 如果你的仓库名是 my-repo，则设置 base: '/my-repo/'
   // 如果使用自定义域名或用户/组织页面，则设置 base: '/'
   base: process.env.GITHUB_PAGES === 'true' ? '/' : '/',
-  optimizeDeps: {
-    exclude: ["bippy/dist/jsx-dev-runtime", "bippy/dist/jsx-runtime"],
-  },
+
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
