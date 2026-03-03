76f7fcf7a944a2ff0e4fe4da241cf79f1209f9c3
Michelle-vr <michvanr@outlook.com>
2026-03-03 13:17:53 +1300

    Update NovaXLogo.tsx

--- src/components/NovaXLogo.tsx
+++ src/components/NovaXLogo.tsx
@@ -9,7 +9,7 @@ interface NovaXLogoProps {
 const NovaXLogo: React.FC<NovaXLogoProps> = ({ size = 120, animate = true }) => {
   // Using the NovaX logo image directly
   // The logo should be placed in /public/novax-logo.png
-  const logoUrl = 'https://i.imgur.com/YourLogoId.png' // You'll replace this with your actual logo URL
+  const logoUrl = 'https://imgur.com/a/CpU6ujC' // You'll replace this with your actual logo URL
 
   return (
     <motion.div
